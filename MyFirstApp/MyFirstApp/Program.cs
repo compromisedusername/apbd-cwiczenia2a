25da816cb12fe6f21397fdc3e93f275849671689
compromisedusername <haldman123@gmail.com>
2024-03-09 16:31:49 +0100

    Add GetAverage

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World2!");
 Console.WriteLine("Hello, World3!");
 Console.WriteLine("Hello, World4!");
+
+double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
+}
